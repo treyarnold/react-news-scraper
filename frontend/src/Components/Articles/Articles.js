1364e2673156e22100e5d5961bb5664c750b8c26
treyarnold <arnold.trey80@gmail.com>
2019-07-06 14:03:14 -0500

    article display done

--- frontend/src/Components/Articles/Articles.js
+++ frontend/src/Components/Articles/Articles.js
@@ -1,26 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import Article from "./Article/Article";
 import classes from "./Articles.module.css";
 
 const db = "http://localhost:3001/articles";
 
 const Articles = () => {
   const [articles, setArticles] = useState({
+    articleData: [],
     hasArticles: false
   });
 
   useEffect(() => {
-    axios.get(db).then((result) => {
-      console.log(result.data.length);
-
-    })
+    console.log(articles.articleData);
+    if (!articles.hasArticles){
+      axios.get(db).then((result) => {
+        if (result.data.length) {
+          setArticles({
+            articleData: result.data,
+            hasArticles: true
+          })
+        }
+      })
+    }
+    console.log(articles.articleData);
   }, [articles])
+
+  let content = <p>loading</p>;
+  if (articles.hasArticles) {
+    content = articles.articleData.map((article) => (
+      <Article key={article._id} data={article} />
+    ))
+  } 
 
   return (
     <main className={classes.main}>
-      <button>Scrape New Articles</button>
-
+      {content}
     </main>
   )
 }
